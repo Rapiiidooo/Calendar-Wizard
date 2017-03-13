c88197dd7eb6725f334e886736bdaeb2385066be
b845548@gmail.com <b845548@octo>
2017-03-13 16:48:00 +0100

    supprime jpg.jpg - HBB

--- e.py
+++ e.py
@@ -1,4 +1,4 @@
-from tkinter import *
+from Tkinter import *
 
 frames=[]
 current = 0
@@ -43,7 +43,72 @@ def update():
 def myfunction(event):
     canvas.configure(scrollregion=canvas.bbox("all"),width=sizex-20,height=sizey-5)
 
-
+def add_item():
+    """
+    add the text in the Entry widget to the end of the listbox
+    """
+    listbox1.insert(tk.END, enter1.get())
+def delete_item():
+    """
+    delete a selected line from the listbox
+    """
+    try:
+        # get selected line index
+        index = listbox1.curselection()[0]
+        listbox1.delete(index)
+    except IndexError:
+        pass
+ 
+def get_list(event):
+    """
+    function to read the listbox selection
+    and put the result in an entry widget
+    """
+    # get selected line index
+    index = listbox1.curselection()[0]
+    # get the line's text
+    seltext = listbox1.get(index)
+    # delete previous text in enter1
+    enter1.delete(0, 50)
+    # now display the selected text
+    enter1.insert(0, seltext)
+def set_list(event):
+    """
+    insert an edited line from the entry widget
+    back into the listbox
+    """
+    try:
+        index = listbox1.curselection()[0]
+        # delete old listbox line
+        listbox1.delete(index)
+    except IndexError:
+        index = tk.END
+    # insert edited item back into listbox1 at index
+    listbox1.insert(index, enter1.get())
+def sort_list():
+    """
+    function to sort listbox items case insensitive
+    """
+    temp_list = list(listbox1.get(0, tk.END))
+    temp_list.sort(key=str.lower)
+    # delete contents of present listbox
+    listbox1.delete(0, tk.END)
+    # load listbox with sorted data
+    for item in temp_list:
+        listbox1.insert(tk.END, item)
+def save_list():
+    """
+    save the current listbox contents to a file
+    """
+    # get a list of listbox lines
+    temp_list = list(listbox1.get(0, tk.END))
+    # add a trailing newline char to each line
+    temp_list = [chem + '\n' for chem in temp_list]
+    # give the file a different name
+    fout = open("chem_data2.txt", "w")
+    fout.writelines(temp_list)
+    fout.close()
+ 
 
 
 
@@ -55,6 +120,19 @@ myframe.grid(row = 2, column=0)
 
 myframe.place(x=0,y=0)
 canvas=Canvas(myframe)
+
+
+
+# create the listbox (note that size is in characters)
+listbox1 = tk.Listbox(root, width=50, height=6)
+listbox1.grid(row=0, column=0)
+ 
+# create a vertical scrollbar to the right of the listbox
+yscroll = tk.Scrollbar(command=listbox1.yview, orient=tk.VERTICAL)
+yscroll.grid(row=0, column=1, sticky=tk.N+tk.S)
+listbox1.configure(yscrollcommand=yscroll.set)
+
+
 
 
 frame=Frame(canvas)
